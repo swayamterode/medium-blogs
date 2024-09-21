286513176b0f3614cbb711bda54222cc7a54dbb6
Swayam Terode <swayamterodex@gmail.com>
2024-09-22 01:14:56 +0530

    refactor: Add delete route for deleting a blog post

--- backend/src/routes/blogs.ts
+++ backend/src/routes/blogs.ts
@@ -1,10 +1,14 @@
 import { Hono } from "hono";
+// Middleware
 import { authMiddleware } from "../middleware/authMiddleware";
+// Controller
 import { createBlog } from "../controller/blogs/createBlogs.controller";
 import { updateBlog } from "../controller/blogs/updateBlogs.controller";
 import { getBulkBlogs } from "../controller/blogs/getBulkBlogs.controller";
 import { getSingleBlog } from "../controller/blogs/getSingleBlog.controller";
+import { deleteBlogPost } from "../controller/blogs/deleteBlog.controller";
 
+// Router
 export const blogRouter = new Hono<{
   Bindings: {
     DATABASE_URL: string;
@@ -15,8 +19,10 @@ export const blogRouter = new Hono<{
   };
 }>();
 
+// Routes
 blogRouter.use("/*", authMiddleware);
 blogRouter.post("/create", createBlog);
 blogRouter.put("/", updateBlog);
 blogRouter.get("/bulk", getBulkBlogs);
 blogRouter.get("/single-blog/:id", getSingleBlog);
+blogRouter.delete("/delete-blog/:id", deleteBlogPost);
